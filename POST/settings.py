aafbfe9efd9ba6778afbcaab03933dd478354dab
as <megumiru175@gmail.com>
2026-07-16 14:43:58 +0900

    Configure ALLOWED_HOSTS for Render

--- POST/settings.py
+++ POST/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xbq^$tg%9&&^##6vor@i3(h#*^^tj9-0d&&xguv-xqs71d8puj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -120,6 +120,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_ROOT = BASE_DIR/"media"
 
 MEDIA_URL = "/media/"

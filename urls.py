63a58878965cc4bd88f6f9db94088cd702c10aa0
as <megumiru175@gmail.com>
2026-07-23 15:02:24 +0900

    Update urls.py

--- urls.py
+++ urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import  static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda request: redirect("/login/")),
     path('admin/', admin.site.urls),
     path("myjob/", include("myjob.urls")),
     path('', include('accounts.urls')),

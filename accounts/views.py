88308c16257b8ef74ef828544339fa53a4b43082
as <megumiru175@gmail.com>
2026-07-28 14:52:08 +0900

    Update vies.py

--- accounts/views.py
+++ accounts/views.py
@@ -24,11 +24,7 @@ class MySignupView(CreateView):
     
 class MyUserView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/new.html'
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['user'] = self.request.user
-        return context
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['users'] = User.objects.exclude(username=self.request.user.username)

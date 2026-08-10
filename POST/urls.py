017d5e3ee773b1c06b7c03106f5f4bbf6e95d369
as <megumiru175@gmail.com>
2026-08-10 13:09:27 +0900

    Update login.html

--- POST/urls.py
+++ POST/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import  static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("myjob/", include("myjob.urls")),
     path('', include('accounts.urls')),
+    path("myjob/", include("myjob.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- settings.py
+++ settings.py
@@ -146,6 +146,6 @@ MEDIA_URL = "/media/"
 
 LOGIN_REDIRECT_URL = "myjob:index"
 
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "accounts:login"  
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
